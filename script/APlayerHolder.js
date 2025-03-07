58bb7ff9fe4c942a7c199541ff6b398694582da9
MikeMing126 <mingmike126@gmail.com>
2025-03-07 20:50:11 +0800

    Site updated: 2025-03-07 20:50:11

--- script/APlayerHolder.js
+++ script/APlayerHolder.js
@@ -1,24 +1,43 @@
 function InitAPlayerHolder() {
-    var flag=0;
-    try
+    //获取全局对象aplayer。
+    //注意判空
+    var ap = window.aplayers && window.aplayers[0];
+
+    if(ap == null)
     {
-        ap=aplayers[0]; //aplayer对象的存放位置挺离谱的
-        flag = 1;
-    }
-    catch
-    {
-        setTimeout(InitAPlayerHolder, 50);//等待aplayer对象被创建（没找到初始化实例的地方只能这样了，这个判断代码是StackOverflow上面扒的（因为自己是个蒟蒻
+        //等待aplayer对象被创建
+        setTimeout(InitAPlayerHolder, 50);
         return;
     }
 
-    if (!flag) 
-        return;
-    
+    //#region 状态保存函数
+    //原基础上加了个检测暂停免得切换页面后爆零(bushi)（指社死）
+    ap.on("pause",function(){
+        sessionStorage.setItem("aPlayerMusicPaused",1);
+        //ap.lrc.hide()
+    });
+    //自带播放暂停时显隐歌词，后面那句可以删，上同
+    ap.on("play",function(){
+        sessionStorage.setItem("aPlayerMusicPaused",0);
+        //ap.lrc.show()
+    });
+
+    //保存音量
+    ap.audio.onvolumechange=function(){sessionStorage.setItem("aPlayerMusicVolume",ap.audio.volume);};
+
+    //Index/Time/Mode 定时保存
+    var saveTimeInterval = 150;
+    setInterval(function(){
+        localStorage.setItem("aPlayerMusicIndex",ap.list.index);
+        sessionStorage.setItem("aPlayerMusicTime",ap.audio.currentTime);
+        sessionStorage.setItem("aPlayerMusicMode",ap.mode);
+    },saveTimeInterval);
+    //#endregion 状态保存函数
+
+    //#region 加载保存进度
     //自带播放暂停时显隐歌词，可以删
     ap.lrc.hide();
-
     document.getElementsByClassName("aplayer-icon-menu")[0].click()
-    //加载保存进度
     //Index
     if(localStorage.getItem("aPlayerMusicIndex")!=null)
     {
@@ -30,7 +49,11 @@ function InitAPlayerHolder() {
     {
         window.musict = sessionStorage.getItem("aPlayerMusicTime");
         ap.setMode(sessionStorage.getItem("aPlayerMusicMode"));
-        if(sessionStorage.getItem("aPlayerMusicPaused")!='1')
+        if(sessionStorage.getItem("aPlayerMusicPaused")==1)
+        {
+            ap.pause();
+        }
+        else
         {
             ap.play();
         }
@@ -55,30 +78,7 @@ function InitAPlayerHolder() {
     {
         ap.audio.volume=Number(sessionStorage.getItem("aPlayerMusicVolume"));
     }
-
-    //#region 状态保存函数
-    //原基础上加了个检测暂停免得切换页面后爆零(bushi)（指社死）
-    ap.on("pause",function(){
-        sessionStorage.setItem("aPlayerMusicPaused",1);
-        //ap.lrc.hide()
-    });
-    //自带播放暂停时显隐歌词，后面那句可以删，上同
-    ap.on("play",function(){
-        sessionStorage.setItem("aPlayerMusicPaused",0);
-        //ap.lrc.show()
-    });
-
-    //保存音量
-    ap.audio.onvolumechange=function(){sessionStorage.setItem("aPlayerMusicVolume",ap.audio.volume);};
-
-    //Index/Time/Mode 定时保存
-    var saveTimeInterval = 200;
-    setInterval(() => {
-        localStorage.setItem("aPlayerMusicIndex",ap.list.index);
-        sessionStorage.setItem("aPlayerMusicTime",ap.audio.currentTime);
-        sessionStorage.setItem("aPlayerMusicMode",ap.mode);
-    },saveTimeInterval);
-    //#endregion 状态保存函数
+    //#endregion 加载保存进度
 }
 
 //执行逻辑
